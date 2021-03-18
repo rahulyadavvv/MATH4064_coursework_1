232d71f34834de3cd836e87d6d65b0bf967c739f
TD600 <80839532+TD600@users.noreply.github.com>
2021-03-18 12:30:58 +0000

    Add files via upload

---
+++ Q2b.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cmath>
+#include "JacobianApproximator.hpp"
+
+double F_1(double x, double y);
+double F_2(double x, double y);
+double* F(double* x);
+double ComputeMatrix1Norm(double** x, double** y,int n);
+
+int main(int argc, char* argv[])
+{
+    double* x;
+    x = new double[2];
+
+    x[0] = 2;
+    x[1] = 2;
+
+    double e = 0.2;
+
+    double** Jac = ApproximateJacobian(x, F_1, F_2, e, 2);
+
+    std::cout << Jac[0][0] << " " << Jac[0][1] << std::endl;
+    std::cout << Jac[1][0] << " " << Jac[1][1] << std::endl;
+
+    std::cout << std::endl;
+
+    double** Jaco = ComputeApproximateJacobian(x, F, 2, e);
+
+    std::cout << Jaco[0][0] << " " << Jaco[0][1] << std::endl;
+    std::cout << Jaco[1][0] << " " << Jaco[1][1] << std::endl;
+
+    std::cout << std::endl;
+
+    double** iJac = ApproximateJacobianInverse(x, F_1, F_2, e, 2);
+
+    std::cout << iJac[0][0] << " " << iJac[0][1] << std::endl;
+    std::cout << iJac[1][0] << " " << iJac[1][1] << std::endl;
+
+    double** ActualJacobian;
+    ActualJacobian = new double*[2];
+    for(int i=0; i<2; i++)
+    {
+        ActualJacobian[i] = new double[2];
+    }
+
+    ActualJacobian[0][0] = 4;
+    ActualJacobian[0][1] = -1;
+    ActualJacobian[1][0] = 4;
+    ActualJacobian[1][1] = 4;
+
+    std::cout << "epsilon" << "  " << "error" << std::endl;
+    for(int i=0; i<6; i++)
+    {
+        std::cout << 0.2/pow(2,i) << "  " << ComputeMatrix1Norm(ActualJacobian, ApproximateJacobian(x, F_1, F_2, 0.2/pow(2,i), 2), 2) << std::endl;
+    }
+
+    return 0;
+}
+
+double F_1(double x, double y)
+{
+    return pow(x,2)-y;
+}
+
+double F_2(double x, double y)
+{
+    return pow(x,2)+pow(y,2)-2;
+}
+
+double* F(double* x)
+{
+    double* y;
+    y = new double[2];
+
+    y[0] = pow(x[0],2)-x[1];
+    y[1] = pow(x[0],2)+pow(x[1],2)-2;
+
+    return y;
+}
+
+double ComputeMatrix1Norm(double** x, double** y,int n)
+{
+    double norm = 0;
+
+    double** z;
+    z = new double*[n];
+    for(int i=0; i<n; i++)
+    {
+        z[i] = new double[n];
+    }
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            z[i][j] = fabs(x[i][j]-y[i][j]);
+        }
+    }
+    
+    double* sumcols;
+    sumcols = new double[n];
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            sumcols[i] += z[j][i];
+        }
+    }
+
+    norm = sumcols[0];
+    for(int i=0; i<n; i++)
+    {
+        if(sumcols[i]>norm)
+        {
+            norm = sumcols[i];
+        }
+    }
+    return norm;
+}
---
+++ TridiagonalSolver.hpp
@@ -0,0 +1,3 @@
+double* SolveTridiagonalSystem(int n, double* lower, double* diagonal, double* upper, double* rhs);
+
+double* Vector(int length);
