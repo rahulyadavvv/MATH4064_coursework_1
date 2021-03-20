cbd96ca44c6eaf648cf940753d3e9aeef1a0acb1
rahulyadavvv <rahul44y@gmail.com>
2021-03-20 14:40:32 +0000

    option 2 update

--- Question_2/option2.cpp
+++ Question_2/option2.cpp
@@ -56,31 +56,67 @@ pVector)
     return pResult;
 }
 
+double ComputeOneNorm(int noRows, double* x)
+{
+    double result = 0.0;
+    for (int i = 0; i < noRows; i++)
+    {
+        result += abs(x[i]);
+    }
+    return result;
+}
+
 int main(int argc, char* argv[]){
 
-    double** x = AllocateMatrix(6, 2);
+    const int xvals = 6;
+    double** x = AllocateMatrix(xvals, 2);
     double* xReal = AllocateVector(2);
 
+    //  set initial x_0 starting value in x
     x[0][0] = 2.0;
     x[0][1] = 2.0;
 
-    for (int i = 0; i < 5; i++) //stop before last row at x_4
+    //  store value for x when F is 0
+    xReal[0] = 1.0;
+    xReal[1] = 1.0;
+
+    for (int i = 0; i < xvals - 1; i++) //stop before last row at x_4
     {
+        //  calculate J(x_i)^(-1)
         double** JxInverse = Inverse2x2(Jacobian(x[i]));
+        //  calculate value of F at x_i
         double* Fx = F(x[i]);
+        //  calculate J(x_i)^(-1)*F(x_i)
         double* product = MultiplyMatrixVector(2, 2, JxInverse, Fx);
 
+        //  calculate x_(i+1) = x_i  - J(x_i)^(-1)*F(x_i)
         x[i + 1][0] = x[i][0] - product[0];
         x[i + 1][1] = x[i][1] - product[1];
-
-        //need to still test
 
         DeallocateVector(product);
         DeallocateVector(Fx);
         DeallocateMatrix(2, JxInverse);
     }
     
-    PrintMatrix(6, 2, x);
+    //  output x_k for k = 0, 1, 2, 3, 4, 5
+    std::cout << "x_k for k = 0, 1, 2, 3, 4, 5:" << std::endl;
+    PrintMatrix(xvals, 2, x);
+
+    double* error = AllocateVector(xvals);
+    for (int i = 0; i < xvals; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            x[i][j] = xReal[j] - x[i][j];
+        }
+        error[i] = ComputeOneNorm(2, x[i]);
+    }
+    
+    //  output error for k = 0, 1, 2, 3, 4, 5
+    std::cout << "error for k = 0, 1, 2, 3, 4, 5:" << std::endl;
+    PrintColVector(xvals, error);
+
+    DeallocateVector(error);
     DeallocateVector(xReal);
-    DeallocateMatrix(5, x);
+    DeallocateMatrix(xvals, x);
 }
