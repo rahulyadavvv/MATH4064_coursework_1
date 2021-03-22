0caf6cc4f26aabd3544d456765a322069f338dff
TD600 <80839532+TD600@users.noreply.github.com>
2021-03-22 17:24:09 +0000

    Delete Thomas directory

--- Thomas/EulerMethod.hpp
+++
@@ -1,6 +0,0 @@
-double* SolveODESystemEuler(double* initial, int max_iter, double* (*pFunction)(double* x), double h);
-
-double* ComputeEulerIteration(int iter, double* initial, double* (*pFunction)(double* x), double h);
-
-double Compute2Norm(double* x, double* y, int n);
-
--- Thomas/TridiagonalSolver.hpp
+++
@@ -1,3 +0,0 @@
-double* SolveTridiagonalSystem(int n, double* lower, double* diagonal, double* upper, double* rhs);
-
-double* Vector(int length);
