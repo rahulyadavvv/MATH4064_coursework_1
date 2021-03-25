bbc3fc45e957f0fcdcb83c224db43e68cd515e0f
rahulyadavvv <rahul44y@gmail.com>
2021-03-25 19:35:17 +0000

    main converted to functions, need to finish RHS function

--- Question_1/option2.cpp
+++ Question_1/option2.cpp
@@ -1,82 +1,63 @@
 #include <iostream>
+#include "general.hpp"
 
-double* AllocateVector(int noRows)
+double* InitialiseMainDiagonal(int length)
 {
-    double* vector;
-    vector = new double [noRows];
-    return vector;
-}
-
-void DeallocateVector(double* vector)
-{
-    delete[] vector;
-}
-
-void PrintVector(int noRows, double* vector)
-{
-    for (int i = 0; i < noRows; i++)
+    double* result = AllocateVector(length);
+    for (int i = 0; i < length; i++)
     {
-        if (noRows == 1)
-        {
-            std::cout << "[ " << vector[i] << " ]";
-        }
-        else if (i == 0)
-        {
-            std::cout << "[ " << vector[i] << " ";
-        }
-        else if (i == noRows - 1) 
-        {
-            std::cout << vector[i] << " ]" << std::endl;
-        }
-        else
-        {
-            std::cout << vector[i] << " ";
-        }   
+        result[i] = 4.0;
     }
+    return result;
+}
+
+double* InitialiseLowerDiagonal(int length)
+{
+    double* result = AllocateVector(length);
+    result[0] = 0.0;
+    for (int i = 1; i < length - 1; i++)
+    {
+        
+        result[i] = 1.0;
+    }
+    result[length - 1] = 2.0;
+    return result;
+}
+
+double* InitialiseUpperDiagonal(int length)
+{
+    double* result = AllocateVector(length);
+    result[0] = 2.0;
+    for (int i = 1; i < length - 1; i++)
+    {
+        
+        result[i] = 1.0;
+    }
+    result[length - 1] = 0.0;
+    return result;
 }
 
 int main(int argc, char* argv[]){
     //  get user to input length and n points
-    int n;
-    double length;
-    std::cout << "specify length and n --" << std::endl;
-    std::cout << "length << ";
-    std::cin >> length;
-    std::cout << "n << ";
-    std::cin >> n;
+    int n = 6;
+    double length = 3.0;
 
     //  calculate and store step size in h 
     double h = length/((double)n);
 
     /* allocate memory for diagonals and assign d = main diagonal, u = upper
     diagonal, l = lower diagonal     */
-    double* d = AllocateVector(n);
-    double* u = AllocateVector(n);
-    double* l = AllocateVector(n);
-
-    // for loop to generate coefficients
-    for (int i = 0; i < n; i++)
-    {
-        d[i] = 4.0;
-        if (i == 0)
-        {
-            l[i] = 0.0;
-            u[i] = 2.0;
-        } else if (i == n-1)
-        {
-            l[i] = 2.0;
-            u[i] = 0.0;
-        } else 
-        {
-            l[i] = 1.0;
-            u[i] = 1.0;
-        }
-    }
+    double* d = InitialiseMainDiagonal(n);
+    double* u = InitialiseUpperDiagonal(n);
+    double* l = InitialiseLowerDiagonal(n);
     
     //  print vectors for upper diagonal, main diagonal and lower diagonal
-    PrintVector(n, u);
-    PrintVector(n, d);
-    PrintVector(n, l);
+    std::cout << "upper diagonal: ";
+    PrintRowVector(n, u);
+    std::cout << "main diagonal:  ";
+    PrintRowVector(n, d);
+    std::cout << "lower diagonal: ";
+    PrintRowVector(n, l);
 
     //  deallocate memory for upper diagonal, main diagonal and lower diagonal
     DeallocateVector(l);
