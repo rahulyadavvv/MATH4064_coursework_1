721fd4e4eefd3e45176a8907e56ef3e96f954c09
olivermoring <80835856+olivermoring@users.noreply.github.com>
2021-03-26 01:05:33 +0000

    Update TridiagonalMatrix.hpp

--- Oli/Question1/TridiagonalMatrix.hpp
+++ Oli/Question1/TridiagonalMatrix.hpp
@@ -1,6 +1,5 @@
 
-double* TridiagSolve(int n, double* array1, double* array2,
-                            double* array3, double* array4);
+double* TridiagSolve(int n, double* vecL, double* vecD,
+                            double* vecU, double* vecRHS);
 
 double* Vector(int length);
-
