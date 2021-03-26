44dbf9e02d18643c9f59fb270d8b3abafa22ff24
olivermoring <80835856+olivermoring@users.noreply.github.com>
2021-03-26 01:04:00 +0000

    Add files via upload

---
+++ Oli/Question1/GeneralFunctions.cpp
@@ -0,0 +1,32 @@
+
+double* Vector(int length)
+{
+    double* vector;
+    vector = new double[length];
+    return vector;
+}
+
+void DeleteVector(double* vector)
+{
+    delete[] vector;
+}
+
+double** Matrix(int n)
+{
+    double** matrix;
+    matrix = new double*[n];
+    for(int i=0; i<n; i++)
+    {
+        matrix[i] = new double[n];
+    }
+    return matrix;
+}
+
+void DeleteMatrix(double** matrix, int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
---
+++ Oli/Question1/TridiagonalMatrix.hpp
@@ -0,0 +1,6 @@
+
+double* TridiagSolve(int n, double* array1, double* array2,
+                            double* array3, double* array4);
+
+double* Vector(int length);
+
