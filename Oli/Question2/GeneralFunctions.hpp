76fd84fc0a8d431ddc95ed03e3a76ad0d85dbdeb
rahulyadavvv <rahul44y@gmail.com>
2021-05-30 14:31:52 +0100

    Revert "Add files via upload"
    This reverts commit cc3826b100c49882582c7130ffd676783b0372f4.

--- Oli/Question2/GeneralFunctions.hpp
+++
@@ -1,8 +0,0 @@
-double* Vector(int length);
-
-void DeleteVector(double* vector);
-
-double** Matrix(int n);
-
-void DeleteMatrix(double** matrix, int n);
-
--- Oli/Question2/Jacobian.hpp
+++
@@ -1,5 +0,0 @@
-double** ComputeApprox(double* x, double* (*pF)(double* x), int n, double epsilon);
-
-double** ComputeInverseApprox(double * x, double* (*pF)(double* x), int n, double epsilon);
-
-void ComputeInverseApprox(double** matrix, double* x, double* (*pF)(double* x), double epsilon, int n);
